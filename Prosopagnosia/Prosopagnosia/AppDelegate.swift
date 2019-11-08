bc173be3be4014941b54aac772e6ccfa0529e92a
summerdtao <summer_d_tao@me.com>
2019-11-08 14:08:49 -0500

    changes to the template

--- Prosopagnosia/Prosopagnosia/AppDelegate.swift
+++ Prosopagnosia/Prosopagnosia/AppDelegate.swift
@@ -3,10 +3,10 @@
 //  Prosopagnosia
 //
 //  Created by 艺 陶 on 2019-11-08.
+//  Copyright © 2019 the prosopagnosia team. All rights reserved.
 //
 
 import UIKit
-import SwiftUI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,15 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Use a UIHostingController as window root view controller.
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UIHostingController(rootView: contentView)
-        self.window = window
-        window.makeKeyAndVisible()
+        // Override point for customization after application launch.
         return true
     }
 
